ea46e234e34fbc4b181af19a8d7ac92733caf86d
arvikachristy <anastasia.arvika@gmail.com>
2017-02-22 19:20:39 +0000

    added policy gradient agent :hurtrealbad:

---
+++ policy gradient/policy_gradient.py
@@ -0,0 +1,148 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import gym
+import universe
+import matplotlib.pyplot as plt
+
+env = gym.make('wob.mini.ClickTest-v0')
+env.configure(remotes=1, fps=5,
+              vnc_driver='go', 
+              vnc_kwargs={'encoding': 'tight', 'compress_level': 0, 
+                          'fine_quality_level': 100, 'subsample_level': 0})
+gamma = 0.99
+
+def discount_rewards(r):
+    """ take 1D float array of rewards and compute discounted reward """
+    discounted_r = np.zeros_like(r)
+    running_add = 0
+    for t in reversed(xrange(0, r.size)):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+class agent():
+    def __init__(self, lr, s_size,a_size,h_size):
+        #These lines established the feed-forward part of the network. The agent takes a state and produces an action.
+        self.state_in= tf.placeholder(shape=[None,s_size],dtype=tf.float32)
+        hidden = slim.fully_connected(self.state_in,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
+        self.output = slim.fully_connected(hidden,a_size,activation_fn=tf.nn.softmax,biases_initializer=None)
+        self.chosen_action = tf.argmax(self.output,1)
+
+        #The next six lines establish the training proceedure. We feed the reward and chosen action into the network
+        #to compute the loss, and use it to update the network.
+        self.reward_holder = tf.placeholder(shape=[None],dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[None],dtype=tf.int32)
+        
+        self.indexes = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
+        self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+
+        self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs)*self.reward_holder)
+        
+        tvars = tf.trainable_variables()
+        self.gradient_holders = []
+        for idx,var in enumerate(tvars):
+            placeholder = tf.placeholder(tf.float32,name=str(idx)+'_holder')
+            self.gradient_holders.append(placeholder)
+        
+        self.gradients = tf.gradients(self.loss,tvars)
+        
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+        self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders,tvars))
+
+
+tf.reset_default_graph() #Clear the Tensorflow graph.
+
+
+def doAction(a, x, y):
+    small_step = 5
+    minY = 125
+    maxY = 285
+    minX = 10
+    maxX = 170
+    if a == 0:
+        return [universe.spaces.PointerEvent(x, y, 0),
+            universe.spaces.PointerEvent(x, y, 1),
+            universe.spaces.PointerEvent(x, y, 0)], x, y
+    elif a == 1:
+        return [universe.spaces.PointerEvent(x, y, 0)], x, y
+    elif a == 2:
+        if y + small_step <= maxY:
+            return [universe.spaces.PointerEvent(x, y + small_step, 0)], x, y + small_step
+    elif a == 3:
+        if x + small_step <= maxX:
+            return [universe.spaces.PointerEvent(x + small_step, y, 0)], x + small_step, y
+    elif a == 4:
+        if y - small_step >= minY:
+            return [universe.spaces.PointerEvent(x, y - small_step, 0)], x, y - small_step
+    elif a == 5:
+        if x - small_step >= minX:
+            return [universe.spaces.PointerEvent(x - small_step, y, 0)], x - small_step, y
+    return [], x, y
+
+myAgent = agent(lr=1e-2,s_size=4,a_size=6,h_size=8) #Load the agent.
+
+total_episodes = 5000 #Set total number of episodes to train agent on.
+max_ep = 999
+update_frequency = 5
+
+init = tf.global_variables_initializer()
+
+# Launch the tensorflow graph
+with tf.Session() as sess:
+    sess.run(init)
+    i = 0
+    total_reward = []
+    total_lenght = []
+    prevX = 80+10
+    prevY = 80+75+50
+        
+    gradBuffer = sess.run(tf.trainable_variables())
+    for ix,grad in enumerate(gradBuffer):
+        gradBuffer[ix] = grad * 0
+        
+    while i < total_episodes:
+        s = env.reset()
+        #TODO SET S into something until s is not
+        running_reward = 0
+        ep_history = []
+        for j in range(max_ep):
+            #Choose either a random action or one from our network.
+            print "HELOOOOO", s
+            a_dist = sess.run(myAgent.output,feed_dict={myAgent.state_in:[s]})
+            #a_dist = [[0.15,0.15,0.15,0.15,0.15,0.25]]
+            a = np.random.choice(np.arange(0,6),p=a_dist[0])
+            print a
+            #a = np.argmax(a_dist == a) #takes the highest value from the array, but why a_dist==a?
+            doAction(a, prevX, prevY)
+            s1,r,d,_ = env.step(a) #Get our reward for taking an action given a bandit.
+            ep_history.append([s,a,r,s1])
+            s = s1
+            running_reward += r
+            if d == True:
+                #Update the network.
+                ep_history = np.array(ep_history)
+                ep_history[:,2] = discount_rewards(ep_history[:,2])
+                feed_dict={myAgent.reward_holder:ep_history[:,2],
+                        myAgent.action_holder:ep_history[:,1],myAgent.state_in:np.vstack(ep_history[:,0])}
+                grads = sess.run(myAgent.gradients, feed_dict=feed_dict)
+                for idx,grad in enumerate(grads):
+                    gradBuffer[idx] += grad
+
+                if i % update_frequency == 0 and i != 0:
+                    feed_dict= dictionary = dict(zip(myAgent.gradient_holders, gradBuffer))
+                    _ = sess.run(myAgent.update_batch, feed_dict=feed_dict)
+                    for ix,grad in enumerate(gradBuffer):
+                        gradBuffer[ix] = grad * 0
+                
+                total_reward.append(running_reward)
+                total_lenght.append(j)
+                break
+
+        
+            #Update our running tally of scores.
+        if i % 100 == 0:
+            print np.mean(total_reward[-100:])
+        i += 1
+
+env.render()
