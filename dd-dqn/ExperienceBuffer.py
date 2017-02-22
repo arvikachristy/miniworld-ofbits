73d3ec1cd7cbe2bb7851c6239ae087f312360e16
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2017-02-22 14:43:44 +0000

    Add Double Dueling DQN implementation based on awjuliani's tutorial

---
+++ dd-dqn/ExperienceBuffer.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+class ExperienceBuffer():
+    def __init__(self, buffer_size = 50000):
+        self.buffer = []
+        self.buffer_size = buffer_size
+    
+    def add(self,experience):
+        if len(self.buffer) + len(experience) >= self.buffer_size:
+            self.buffer[0:(len(experience)+len(self.buffer))-self.buffer_size] = []
+        self.buffer.extend(experience)
+            
+    def sample(self,size):
+        return np.reshape(np.array(random.sample(self.buffer,size)),[size,5])
